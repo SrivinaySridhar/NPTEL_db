1c6c2f16d51bf4e98c70b8330661012515b8dd3b
Srivinay <srivinay.sridhar@gmail.com>
2023-02-09 16:02:27 +0530

    Updated course & enrolment table with constraints

--- Code/db.py
+++ Code/db.py
@@ -1,9 +1,10 @@
-from sqlalchemy import create_engine, Column, String, Integer, Float, Boolean, Enum, Date, DateTime, ForeignKey, CheckConstraint
+from sqlalchemy import create_engine, Column, String, Integer, Float, Boolean, Enum, Date, DateTime
+from sqlalchemy import ForeignKey, CheckConstraint, UniqueConstraint
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 import datetime as dt
 
 #Creating an engine connecting to sqlite database
-engine = create_engine("sqlite:///courses.db", echo = True)
+engine = create_engine("sqlite:///D:\\NPTEL Internship\\SQLAlchemy\\NPTEL_db\\Databases\\courses2.db", echo = True)
 
 #Declarative style schema definition
 Base = declarative_base()
@@ -31,15 +32,17 @@ class User(Base):
     country = Column(String(), nullable = False)
     state = Column(String(), nullable = False)
     city = Column(String(), nullable = False)
-    pincode = Column(Integer(6)) #Nullable because users enter only during registration
+    pincode = Column(Integer()) #Nullable because users enter only during registration
     qualification = Column(Enum(), nullable = False)
-    graduation_year = Column(Date(), nullable = False) #YYYY
+    graduation_year = Column(Integer(), nullable = False) #YYYY
     profession = Column(Enum(), nullable = False)
 
     #Following fields for profession = Faculty/Students
     college_name = Column(Enum()) #Look into the table that stores local_chapter to college name - Need the enum values - NPTEL team
+
     #The following can be derived from "college_name": 
     #"college_country", "college_state", "college_city", "local_chapter", "college_id"
+
     department = Column(String()) #Possible to make it Enum()? from NPTEL team
 
     #Following fields for profession = Students
@@ -47,7 +50,7 @@ class User(Base):
     study_year = Column(Integer())
     scholarship = Column(Boolean())
 
-    #Following fields for profession = Employees
+    #Following fields for profession = Employed
     employer = Column(String())
     designation = Column(String())
 
@@ -57,7 +60,8 @@ class User(Base):
     last_updated = Column(DateTime()) #Nullable? Can we somehow get this data?
 
     #Relationships
-    enrolments = relationship("Enrolment")
+    courses_enroled = relationship("Enrolment")
+
     #Need the enum values for all the columns
     __table_args__ = (CheckConstraint(gender.in_(['M', 'F', 'O', None])), 
                       CheckConstraint(profession.in_(["student", "faculty", "employed", "other"])), 
@@ -73,36 +77,41 @@ class Course(Base):
     unique_course_id = Column(String(), nullable = False) #Alnum dtype
     course_run_id = Column(String(), primary_key = True, nullable = False) #Alnum dtype
     name = Column(String(), nullable = False)
-    exam_date = Column(Date()) #Nullable? Do we have courses that do not have exams? Only one exam date for a course_run_id
-    #year derived from exam date if needed
-    term = Column(Enum(), nullable = False) #derived First half and second half
+    discipline = Column(Enum(), nullable = False)
+    category = Column(Enum(), nullable = False)
+    exam_date = Column(Date()) #Nullable? 
+
+    #The following can be derived from "college_name": "year" and "term"(First half of the year and second half of the year)
+
     #The duration of the course is available in assignment mapping sheet - Need to join with courses data and input here
-    duration = Column(Integer(), nullable = False) #Changed to Enum because we know it can be 4/8/12 weeks course
+    duration = Column(Integer(), nullable = False)
     faculty = Column(String(), nullable = False) 
     institute = Column(Enum(), nullable = False) #Enum dtype because it can be one of the limited set of universities - NPTEL
     coordinating_institute = Column(Enum(), nullable = False) #Enum dtype because it can be one of the limited set of universities - NPTEL
-    
-    # #Discuss and decide whether the following fields should be added
-    # category = Column(Enum(), nullable = False) #Can be New/Rerun - Add
-    # course_status = Column(Enum(), nullable = False) #What is this? Can be op/uplc Understand and Rename 
-    # is_fdp = Column(Boolean(), nullable = False) #Should we keep this here or in exam_registrations table? What does this mean?
-    # discipline = Column(Enum(), nullable = False) #Should we add this or derive from course_run_id? Separate
+    course_status = Column(Enum(), nullable = False) #What is this? Can be op/uplc Understand and Rename 
+    is_fdp = Column(Boolean(), nullable = False) #Should we keep this here or in exam_registrations table? What does this mean?
 
     #Relationships
-    enrolments = relationship("Enrolment")
+    students_enroled = relationship("Enrolment")
     # assignments = relationship("Assignment") 
+
+    #Need the enum values for all the columns
+    __table_args__ = (CheckConstraint(category.in_(['New', 'Rerun'])), 
+                      CheckConstraint(duration.in_([4, 8, 12])), 
+                      CheckConstraint(course_status.in_(["op", "uplc"]))) #Rename this if possible
 
 #Enrolments table
 class Enrolment(Base):
     __tablename__ = "enrolments"
 
-    #How to enforce that a user can register to a particular course only one time?
-    #CheckConstraint on Table - for a course_run_id, user_id should be unique?
     enrolment_id = Column(Integer(), primary_key = True)
     user_id = Column(Integer(), ForeignKey("users.user_id"), nullable = False)
     course_run_id = Column(String(), ForeignKey("courses.course_run_id"), nullable = False) #Alnum dtype
-    date = Column(DateTime(), default = dt.datetime.now(), nullable = False)
-    #first_seen? - Useful
+    date = Column(DateTime(), nullable = False) #default = dt.datetime.now()?
+    first_seen = Column(DateTime(), nullable = False) #Data available?
+
+    #UniqueConstraint to make sure each user can enrol to a particular course offered in a term only once
+    __table_args__ = (UniqueConstraint('user_id', 'course_run_id', name='_user_course_run_uc'),)
 
 #Creating the tables by calling the below function
 Base.metadata.create_all(engine)
