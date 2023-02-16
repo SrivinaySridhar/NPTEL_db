870414fac04272e5b1d2c8b22595f6631448f8dd
Srivinay <srivinay.sridhar@gmail.com>
2023-02-16 14:12:05 +0530

    Code for deriving duration column

--- Code/clean_courses.py
+++ Code/clean_courses.py
@@ -1,39 +1,54 @@
 import pandas as pd
 
+COURSES_URL = '..\Data\Course_details_2022 - Courses_2022.csv'
+DISCIPLINE_URL = '..\Data\Course_details_2022 - discipline.csv'
+DURATION_URL = '..\Data\Course_details_2022 - duration.csv'
+
 def Load_Data(file_name):
     data = pd.read_csv(file_name)
     return data
 
-# A function to extract the duration of the courses
-def extract_duration(df):
-    pass
-
 # A function to load the discipline mapping file and return a dictionary of the mapping 
 def discipline_map(file_name):
-    df = Load_Data(file_name)
+    # Load the discipline data
+    df_discipline = Load_Data(file_name)
 
     # Drop the diciplineid column
-    df = df.loc[:, df.columns.intersection(['Disc_short_code','Discipline Name'])]
+    df_discipline = df_discipline[['Disc_short_code','Discipline Name']]
+
+    # Drop duplicate values
+    df_discipline = df_discipline.drop_duplicates(subset = ['Disc_short_code'], keep = 'first')
 
     # Convert the df into a dictionary with the indices as the short code and the values as the full name
-    discipline_mapping = df.set_index('Disc_short_code').T.to_dict('records')
+    discipline_mapping = df_discipline.set_index('Disc_short_code').T.to_dict('records')
 
-    return discipline_mapping
+    # Return the dictionary that is sitting inside this list
+    return discipline_mapping[0]
 
 # A function to load the duration mapping file and return a dictionary of the mapping 
 def duration_map(file_name):
-    df = Load_Data(file_name)
+    # Load the duration data
+    df_duration = Load_Data(file_name)
 
-    # Extract the duration of the courses
-    df['_duration'] = extract_duration(df)
+    # Three formats for the "Duration" column:
+    # 1. "12 Weeks" - 
+    # 2. "8 Weeks + 4 Weeks(New)"
+    # 3. "4 Week(rerun) + 4 Week (New)"
+    
+    # The following function outputs different integer values according to the different formats
+    # We can apply this to the "Duration" column, and can extract the integer values of the duration of each course
+    func = lambda x : int(x[0:2]) + int(x[16:18]) if len(x) > 24 else (int(x[0:2]) + int(x[10:12]) if len(x) > 9 else int(x[0:2]))
+
+    df_duration['_duration'] = df_duration['Duration'].apply(func)
 
     # Drop the diciplineid column
-    df = df.loc[:, df.columns.intersection(['Course Id','_duration'])]
+    df_duration = df_duration[['NOCCourseId','_duration']]
 
     # Convert the df into a dictionary with the indices as the short code and the values as the full name
-    duration_mapping = df.set_index('Course Id').T.to_dict('records')
+    duration_mapping = df_duration.set_index('NOCCourseId').T.to_dict('records')
 
-    return duration_mapping
+    # Return the dictionary that is sitting inside this list
+    return duration_mapping[0]
 
 #Read and clean the data before inserting into the database 
 def prepare_course_data(file_name):
@@ -51,7 +66,7 @@ def prepare_course_data(file_name):
     
     # Need to  derive '_discipline' and '_duration' columns. Note: Derived columns start with an '_'
 
-    # Generate the 'discipline' column
+    # Generate the '_discipline' column
     # Need to derive the short form of the discipline from the 'course_run_id'
 
     # Format of course_run_id: noc<YY>-<DD><NN(N)>
@@ -62,19 +77,25 @@ def prepare_course_data(file_name):
     df["_discipline_short"]= df["course_run_id"].str.slice(start, stop, step)
     
     # Load the discipline map dictionary
-    discipline_mapping = discipline_map('..\Data\Course_details_2022 - discipline.csv')
+    discipline_mapping = discipline_map(DISCIPLINE_URL)
 
     # Use the dictionary to derive the "_discipline" column
     df["_discipline"] = df['_discipline_short'].map(discipline_mapping)
 
-    # Need to add code to generate the 'duration' column
-    # ...
+    # Generate the '_duration' column
+    # Use the 'duration_map(file_name)' function to get the mapping of the 'NOCCourseId' to '_duration
+
+    # Load the duration map dictionary
+    duration_mapping = duration_map(DURATION_URL)
+
+    df['_duration'] = df['course_run_id'].map(duration_mapping)
     
     # Storing only the relevant columns and dropping the rest
-    df = df.loc[:, ['unique_course_id', 'course_run_id', 'name', 'discipline', 'category', 'exam_date', 'duration', 'faculty', 
+    df = df.loc[:, ['unique_course_id', 'course_run_id', 'name', '_discipline', 'category', 'exam_date', '_duration', 'faculty', 
                     'institute', 'coordinating_institute', 'course_status', 'is_fdp']]
     
     # Returning the dataframe
     return df
 
-prepare_course_data()
+data = prepare_course_data(COURSES_URL)
+data.head()
